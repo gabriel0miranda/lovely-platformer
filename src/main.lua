6ba7dd026b0505cf9b1618e9279c76d95ef69a62
Gabriel Miranda <gabriel.miranda@discente.ufg.br>
2025-04-29 23:50:23 -0300

    [ADD] Adicionando bases do ECS

--- src/main.lua
+++ src/main.lua
@@ -1,47 +1,15 @@
-function love.keypressed(key)
-    if key=="up" and player.playerCanJump then
-        player.playerCanJump = false
-        player.body:applyForce(0,-1000)
-        player.jumpCooldown = 0.5
-    end
-end
+local World = require ("world")
+local Component = require ("component")
+local System = require ("system")
+
 
 function love.load()
-    -- Creating the map
-    world = require("world")
-    map = require("map")
-    world:createWorld()
-    map:createMap(world.world)
-    -- Creating the player
-    player = require("player")
-    player:createPlayer(world.world)
-    --Creating block
-    block = {}
-    block.body = love.physics.newBody(world.world,200,550,"dynamic")
-    block.shape = love.physics.newRectangleShape(0,0,100,50)
-    block.fixture = love.physics.newFixture(block.body,block.shape,0.4)
-    -- Setting up graphics
-    love.graphics.setBackgroundColor(0.30, 0.66, 0.87)
-    love.window.setMode(500,500)
 end
 
 function love.update(dt)
-    world.world:update(dt)
-    player.jumpCooldown = math.max(player.jumpCooldown-dt, 0)
-    if player.jumpCooldown == 0 then
-        player.playerCanJump = true
-    end
-
-    if love.keyboard.isDown("right") then
-        player.body:applyForce(50,0)
-    elseif love.keyboard.isDown("left") then
-        player.body:applyForce(-50,0)
-    end
+    World:update(dt)
 end
 
 function love.draw()
-    map:drawMap()
-    player:drawPlayer()
-    love.graphics.setColor(0.20,0.20,0.20)
-    love.graphics.polygon("fill",block.body:getWorldPoints(block.shape:getPoints()))
+    World:draw()
 end
